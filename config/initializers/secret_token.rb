c76d60ab4ed4a2b51a104c4b705d60290c839322
Ason <asontrack@gmail.com>
2010-10-27 18:25:45 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fa::Application.config.secret_token = '62b827bed64c993f84d695b85eccb000cd73f0cd686451db1f6a18e7f4a31598fb1c937917ee8ebe2489fa8824009754dd044b5b7d432cea526a6f7374488d7b'
